d0acbe8b9fc5f451f1016319105896ce5a23f41f
vietanh2k <vietanha1k31@gmail.com>
2020-09-22 22:02:56 +0700

    abcd

---
+++ abc.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string tong2songuyen(string a,string b)
+{	
+	string res="";
+    while(a.length() < b.length()) a="0"+a;
+    while(b.length() < a.length()) b="0"+b;
+    int carry=0;
+    for(int i=a.length()-1;i>=0;i--)
+    {
+        int tmp=a[i]-48 + b[i]-48 + carry;
+        carry=tmp/10;
+        tmp=tmp%10;
+        res=(char)(tmp+48)+res;
+    }
+    if(carry>0) res="1"+res;
+    return res;	
+};
+int main(int argc, const char * argv[])
+{
+    string a, b; long n;
+    cin>>a>>b;
+    cout<<tong2songuyen(a,b);
+}
