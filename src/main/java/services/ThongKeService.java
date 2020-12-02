2cd3c83af0a1cb60d95fb3c861e76c8fa505ffb0
messithanh2k <thanhgia10x2511@gmail.com>
2020-12-02 21:00:56 +0700

    update ThongKeService and adminpage

--- src/main/java/services/ThongKeService.java
+++ src/main/java/services/ThongKeService.java
@@ -44,6 +44,35 @@ public class ThongKeService {
         conn.close();
         return tk;
     }
+    
+     public static ThongKe getThongKeQuy(int quy) throws SQLException, ClassNotFoundException {
+        Connection conn = MySqlConnection.getMySqlConnection();
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM kien_nghi WHERE QUARTER(ngayPhanAnh) = " + quy +" ");
+        Statement st1 = conn.createStatement();
+        ResultSet rs1 = st1.executeQuery("SELECT DISTINCT phanLoai FROM kien_nghi WHERE QUARTER(ngayPhanAnh)= " + quy +" ");
+        int CacLoai = 0;
+        int SoLuong = 0;
+        int ChuaTraLoi = 0;
+        int DaPhanLoai = 0;
+        int DaTraLoi;
+        int ChuaPhanLoai;
+        while(rs1.next()) {
+            CacLoai++;
+        }
+        while(rs.next()) {
+            SoLuong++;
+            if (rs.getString("trangThai").equals("chưa trả lời"));
+                ChuaTraLoi++;
+            if (rs.getString("phanLoai")!=null) 
+                DaPhanLoai++;
+        }
+        DaTraLoi = SoLuong - ChuaTraLoi;
+        ChuaPhanLoai = SoLuong - DaPhanLoai;
+        ThongKe tk = new ThongKe(SoLuong, DaTraLoi, ChuaTraLoi, CacLoai, DaPhanLoai, ChuaPhanLoai);
+        conn.close();
+        return tk;
+    }
 }
     
 
