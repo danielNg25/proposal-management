6f931c887b2dcfe04cce10026c65d2432b9d94e3
vietanh2k <vietanha1k31@gmail.com>
2020-12-11 21:28:49 +0700

    update phanhoiservice

--- src/main/java/services/PhanHoiService.java
+++ src/main/java/services/PhanHoiService.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import models.KienNghi;
 import models.PhanHoi;
+import models.PhanHoiForUser;
 
 /**
  *
@@ -49,6 +50,25 @@ public class PhanHoiService {
             String noiDung = rs.getString("noiDung");
  
             PhanHoi phanHoi = new PhanHoi(id, noiDung);
+            listPhanHoi.add(phanHoi);
+        }
+        conn.close();
+        return listPhanHoi;
+    }
+    public static List<PhanHoiForUser> getPhanHoiForUser(int nguoigui_id) throws SQLException, ClassNotFoundException{
+        Connection conn = MySqlConnection.getMySqlConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT kien_nghi.tieuDe, phan_hoi.noiDung FROM phan_hoi,kien_nghi,ph_kn"
+                    + "WHERE phan_hoi.id = ph_kn.phan_hoi_id AND kien_nghi.id = ph_kn.kien_nghi_id"
+                    + "AND kien_nghi.nguoigui_id = '" + nguoigui_id + "'");
+               if (rs == null) {
+            return null;
+        }
+        List<PhanHoiForUser> listPhanHoi = new ArrayList<>();
+        while (rs.next()) {
+            String tieuDeKienNghi = rs.getString("tieuDe");
+            String noiDungPhanHoi = rs.getString("noiDung");
+            PhanHoiForUser phanHoi = new PhanHoiForUser(nguoigui_id, tieuDeKienNghi, noiDungPhanHoi);
             listPhanHoi.add(phanHoi);
         }
         conn.close();
